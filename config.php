d681703cae6330a8405a32a8139349496a60fbbf
Ruan Castro <ruancastro15@gmail.com>
2021-07-16 16:04:29 -0300

    Adicionando o backend com php, recebendo os dados via POST e colocando no banco de dados MySQL local, excluindo o campo telefone (deixando comentado)

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+ // ou inicializo assim
+ //$pdo = new PDO("mysql:dbname=test;host=localhost","root","");
+
+ // ou assim, é mesma coisa 
+ $db_name = 'test';
+ $db_host ='localhost';
+ $db_user ='root';
+ $db_pass = '';
+
+ $pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host,$db_user,$db_pass);

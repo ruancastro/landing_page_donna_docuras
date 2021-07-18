ca3c846f86e3a1b5a461aae8efeb3e92e84ca30e
Ruan Castro <ruancastro15@gmail.com>
2021-07-18 08:40:47 -0300

    adicionando form netify

---
+++ meexclua/index.ts
@@ -0,0 +1,17 @@
+/* function hello(person){
+    return ""Ola, "" + person;
+    }
+    let user = ""Gama"";
+    const msg = hello(user);
+    console.log(msg)" */
+
+function print(){
+    enum Estado {
+    SP,
+    SC,
+    SE,
+    }
+    let uf: string = Estado[3];
+    console.log(uf);
+    }
+    print()
